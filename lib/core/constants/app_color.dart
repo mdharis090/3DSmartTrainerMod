2a771c87a61620a58e1f3f49623750ac58a6973a
mdharis090 <harisnazeer090@gmail.com>
2026-05-13 10:56:13 +0500

    add by costant color

---
+++ lib/core/constants/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6C5CE7);    // Purple
+  static const Color secondary = Color(0xFF00CEC9);  // Teal
+  static const Color background = Color(0xFF0F1226); // Dark background
+  static const Color cardBackground = Color(0xFF1B1F3B);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFA0A4C0);
+  static const Color gradientStart = Color(0xFF6C5CE7);
+  static const Color gradientEnd = Color(0xFF00CEC9);
+}
